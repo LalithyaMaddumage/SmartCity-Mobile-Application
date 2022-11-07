d9fd44f5df0f2e677efb1d7cc575060a2f9d83ab
LalithyaMaddumage <sandeepanilalithya155@gmail.com>
2022-11-07 23:11:50 +0530

    Gas Station details adding part done

--- src/components/gasSection/addGassDetails.js
+++ src/components/gasSection/addGassDetails.js
@@ -1,12 +1,18 @@
-import { Text, View } from 'react-native'
-import React, { Component } from 'react'
+import { View, Text } from 'react-native'
+import React from 'react'
 
-export default class AddGassDetails extends Component {
-  render() {
-    return (
-      <View>
-        <Text>A</Text>
-      </View>
-    )
-  }
+export default function AddGassDetails() {
+    const [stationName, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [from , setTimeFromSelected] = useState ("");
+    const [to , setTimeToSelected] = useState ("");
+    const [availability , setAvaialability] = useState ("");
+    const [contact, setContact] = useState("");
+
+
+  return (
+    <View>
+      <Text>addGassDetails</Text>
+    </View>
+  )
 }
