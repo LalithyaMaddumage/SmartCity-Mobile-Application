375a3c268085453d9883712cb5b7900615245d23
LalithyaMaddumage <sandeepanilalithya155@gmail.com>
2022-11-07 23:34:18 +0530

    Gas Station details adding part changed

--- src/components/gasSection/addGassDetails.js
+++ src/components/gasSection/addGassDetails.js
@@ -9,10 +9,43 @@ export default function AddGassDetails() {
     const [availability , setAvaialability] = useState ("");
     const [contact, setContact] = useState("");
 
+    
+    const sendData = async() =>{
+        const newFuel={
+            stationName,
+            location,
+            from,
+            to,
+            petrol,
+            diesel,
+            contact
+        }
+
+        let response=await FuelAdd (newFuel);
+
+        console.log("data add",response);
+
+
+        if(response.ok){
+            ToastAndroid.show(
+
+                'Fuel Station Successfully Added..', ToastAndroid.SHORT
+              );
+
+            
+        }
+
+
+    }
+
 
   return (
     <View>
-      <Text>addGassDetails</Text>
+      <Text>addGassDetails
+
+
+
+      </Text>
     </View>
   )
 }
